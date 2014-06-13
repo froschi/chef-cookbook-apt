2f3dac9a66669ff32679fd5fa70303ee69a7c11b
Thorsten Fischer <thorsten@froschi.org>
2014-06-13 15:26:29 +0200

    Merged resources and providers for repo and key into repo.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- providers/repo.rb
+++ providers/repo.rb
@@ -1,5 +1,29 @@
+
+def apt_key_fingerprints
+  so = Mixlib::ShellOut.new('apt-key finger').run_command
+  so.stdout.split(/\n/).map do |t|
+    if z = t.match(/^ +Key fingerprint = ([0-9A-F ]+)/)
+      z[1].split.join
+    end
+  end.compact
+end
+
+def apt_key_from_keyserver(key, keyserver)
+  execute "apt-key-install-#{key}" do
+    command "apt-key adv --keyserver #{keyserver} --recv #{key}"
+    action :run
+    not_if do
+      apt_key_fingerprints.any? do |fingerprint|
+        fingerprint.end_with?(key.upcase)
+      end
+    end
+  end
+end
+
 action :add do
-  log "Adding apt repo '#{new_resource.name}'."
+  log "Adding apt repo '#{new_resource.name}' with key '#{new_resource.key}'."
+
+  apt_key_from_keyserver(new_resource.key, new_resource.keyserver)
 
   execute "apt-get-update" do
     command "/usr/bin/apt-get update"
--- resources/repo.rb
+++ resources/repo.rb
@@ -11,3 +11,6 @@ attribute :url, :kind_of => String, :required => true
 attribute :name, :kind_of => String, :name_attribute => true
 attribute :suite, :kind_of => String, :default => node['lsb']['codename']
 attribute :component, :kind_of => String, :default => 'main'
+
+attribute :keyserver, :kind_of => String, :default => "keyserver.ubuntu.com"
+attribute :key, :kind_of => String, :default => nil, :required => true
