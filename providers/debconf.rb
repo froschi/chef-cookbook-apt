2398d705a67a58d302e44ba5f09c97766a33d5f3
Thorsten Fischer <thorsten@froschi.org>
2015-10-29 17:21:13 +0100

    Removed some superfluous code.
    Signed-off-by: Thorsten Fischer <thorsten@froschi.org>

--- providers/debconf.rb
+++ providers/debconf.rb
@@ -1,24 +1,3 @@
-#def apt_key_fingerprints
-#  so = Mixlib::ShellOut.new('apt-key finger').run_command
-#  so.stdout.split(/\n/).map do |t|
-#    if z = t.match(/^ +Key fingerprint = ([0-9A-F ]+)/)
-#      z[1].split.join
-#    end
-#  end.compact
-#end
-#
-#def apt_key_from_keyserver(key, keyserver)
-#  execute "apt-key-install-#{key}" do
-#    command "apt-key adv --keyserver #{keyserver} --recv #{key}"
-#    action :run
-#    not_if do
-#      apt_key_fingerprints.any? do |fingerprint|
-#        fingerprint.end_with?(key.upcase)
-#      end
-#    end
-#  end
-#end
-
 def apt_debconf_values_add(package, key, option, value)
   execute "debconf-set-selection-#{key}" do
     command "echo #{package} #{key} #{option} #{value} | debconf-set-selections"
